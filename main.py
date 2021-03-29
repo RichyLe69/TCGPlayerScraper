51e87a4209d49456bfda7c2719c2c40898c05ad5
richardle17 <richardle17@yahoo.com>
2021-03-29 11:06:58 -0700

    3.29.2021 - collection - 40.9 / 78.4

--- main.py
+++ main.py
@@ -14,5 +14,9 @@ if __name__ == "__main__":
 
 
 # TODO
+# somehow click 50 quantity, near mint, listings without photos
+# somehow incorporate 'collection-' and 'buylist-' into name of file
+# somehow get it synced to a mysql/db
+# somehow get it to run on its own
 # reorganize cardlists by my current binder placements
 # Update buylist with new stuff
