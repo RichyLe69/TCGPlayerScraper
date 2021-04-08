59d5579025a89ce502ff2cd1bb3846ba2278fc55
richardle17 <richardle17@yahoo.com>
2021-04-07 19:49:01 -0700

    Reduced timer for init

--- utils.py
+++ utils.py
@@ -92,7 +92,7 @@ def scrape_website(collection_data_yaml, name):
     lowest_listed_price_total = 0
     lowest_listed_price = 0
     first = True
-    timer = 9
+    timer = 5
     for card in collection_data_yaml:
         url = collection_data_yaml[card]['url']
         condition_edition = collection_data_yaml[card]['edition']
