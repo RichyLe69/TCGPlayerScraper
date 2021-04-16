71ae458455fe742f561e0e7b111f825838f11f48
richardle17 <richardle17@yahoo.com>
2021-04-15 19:56:49 -0700

    Updated Lists: Lightsworn, teledad, plants buylists

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ from utils import scrape_website, sort_market_prices, append_console_to_txt
 import time
 
 # My List of Collections
-yaml_name = {'decks/buylist.yaml', 'decks/collection.yaml'}
+# yaml_name = {'decks/buylist.yaml', 'decks/collection.yaml'}
 # yaml_name = 'decks/new-collection-wip.yaml'
-#
+
+
+split_lists = {'decks/buylist_lightsworn.yaml',
+               'decks/buylist_teledad.yaml',
+               'decks/buylist_plants.yaml',
+               'decks/buylist_extras.yaml'}
 
 if __name__ == "__main__":
 
-    for card_list in yaml_name:
+    for card_list in split_lists:
         my_collection = Collection(card_list)
         data = my_collection.get_yaml_data()
         name = my_collection.get_yaml_name()
@@ -21,6 +26,12 @@ if __name__ == "__main__":
 
 # TODO
 # then make yaml file of just deck nodes.
+# 1 total buylist
+#       split buylist: retro format staples, high rarity singles
+# 1 total collection
+#       split collection: ultimate deck binder, extra deck binder, old school binder, deck core binder
+
+
 # somehow get it synced to a mysql/db
 # somehow get it to run on its own on cloud
 # reorganize cardlists by my current binder placements
