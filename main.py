3af10d56bd8e4b7efb0d95cba0723058ea1a6bfa
richardle17 <richardle17@yahoo.com>
2021-04-19 18:46:02 -0700

    4.19.2021 - SPLIT collections

--- main.py
+++ main.py
@@ -4,17 +4,20 @@ import time
 
 # My List of Collections
 # yaml_name = {'decks/new-collection-wip.yaml'}
-yaml_name = {'decks/buylist.yaml',  'decks/collection.yaml'}
+yaml_name = {'decks/buylist.yaml', 'decks/collection.yaml'}
 
+split_lists = {'decks/split_lists/buylist_lightsworn.yaml',
+               'decks/split_lists/buylist_teledad.yaml',
+               'decks/split_lists/buylist_plants.yaml',
+               'decks/split_lists/buylist_extras.yaml'}
 
-split_lists = {'decks/buylist_lightsworn.yaml',
-               'decks/buylist_teledad.yaml',
-               'decks/buylist_plants.yaml',
-               'decks/buylist_extras.yaml'}
+split_collections = {'decks/split_lists/collection_deck_builder.yaml',
+                     'decks/split_lists/collection_extra_deck.yaml',
+                     'decks/split_lists/collection_old_school.yaml'}
 
 if __name__ == "__main__":
 
-    for card_list in yaml_name:
+    for card_list in split_collections:
         my_collection = Collection(card_list)
         data = my_collection.get_yaml_data()
         name = my_collection.get_yaml_name()
